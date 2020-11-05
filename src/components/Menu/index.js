091696926e47697e19adff324089148abc1715ce
Cicero <cicero.santos@tivit.com>
2020-11-05 17:43:26 -0300

    colocando react slick para fazer o slider

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png'
 import { Link } from 'react-router-dom';
-import Button from '../Button'
-// import ButtonLink from './components/ButtonLink'
-import  './Menu.css';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-          <a to="/">
-            <img className="Logo" src={Logo} alt="AliceFlix logo"/>
-          </a>
-          <Button as={Link} className="ButtonLink" to="/cadastro/video" label="Novo vídeo2">
-            Novo vídeo
-          </Button>
-        </nav>
-    );
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
